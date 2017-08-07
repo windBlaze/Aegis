0904751e6113bf1edc02ae7277e8320e012f900a
Théo <trigas@ulb.ac.be>
2017-08-07 17:16:30 +0200

    sha256

---
+++ Aegis/BridgingHeader.h
@@ -0,0 +1,15 @@
+//
+//  BridgingHeader.h
+//  Aegis
+//
+//  Created by Théo Rigas on 7/08/17.
+//  Copyright © 2017 Black Monkeys Corporation. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <CommonCrypto/CommonHMAC.h>
+
+
+#endif /* BridgingHeader_h */
---
+++ Aegis/SHA256.swift
@@ -0,0 +1,39 @@
+//
+//  SHA256.swift
+//  Aegis
+//
+//  Created by Théo Rigas on 7/08/17.
+//  Copyright © 2017 Black Monkeys Corporation. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    
+    func sha256() -> String{
+        if let stringData = self.data(using: String.Encoding.utf8) {
+            return hexStringFromData(input: digest(input: stringData as NSData))
+        }
+        return ""
+    }
+    
+    private func digest(input : NSData) -> NSData {
+        let digestLength = Int(CC_SHA256_DIGEST_LENGTH)
+        var hash = [UInt8](repeating: 0, count: digestLength)
+        CC_SHA256(input.bytes, UInt32(input.length), &hash)
+        return NSData(bytes: hash, length: digestLength)
+    }
+    
+    private  func hexStringFromData(input: NSData) -> String {
+        var bytes = [UInt8](repeating: 0, count: input.length)
+        input.getBytes(&bytes, length: input.length)
+        
+        var hexString = ""
+        for byte in bytes {
+            hexString += String(format:"%02x", UInt8(byte))
+        }
+        
+        return hexString
+    }
+    
+}
